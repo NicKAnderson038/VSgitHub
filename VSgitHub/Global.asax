7e615d1192ed0a91bc49bc70220f342e12e7000c
Nick Anderson <andersonnick038@gmail.com>
2016-11-21 17:34:20 -0800

    Add project files.

---
+++ VSgitHub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VSgitHub.MvcApplication" Language="C#" %>
---
+++ VSgitHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VSgitHub.Startup))]
+namespace VSgitHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
